18103bc002c442d7bb5328964e39e1c6bf39974d
Suraj Choudhary <jaatsuraj022@gmail.com>
2025-06-20 23:13:52 +0530

    Updated Main.java with new message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GitHub from Java!");
+        System.out.println("Updated line from Suraj! 🔥");
+
     }
 }
